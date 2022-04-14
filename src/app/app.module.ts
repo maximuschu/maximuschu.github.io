cea28a49bfbe63088123bb07fc631e0cfa4a2915
maximuschu <maximuschu@utexas.edu>
2022-04-14 02:06:46 -0500

    Main Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent}, 
+  {path: 'resume', component: ResumeComponent},
+  {path: '', component: HomeComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ResumeComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], 
+  exports: [RouterModule]
 })
 export class AppModule { }
